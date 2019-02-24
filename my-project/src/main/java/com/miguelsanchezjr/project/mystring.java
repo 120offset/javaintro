adc6e933c1389daede25e5b2d77ee5d19ab72c70
120offset <miguel.sanchez.jr@outlook.com>
2019-02-24 17:55:53 -0600

    First commit

---
+++ my-project/src/main/java/com/miguelsanchezjr/project/mystring.java
@@ -0,0 +1,15 @@
+package com.miguelsanchezjr.project;
+
+/*Learning the concept of using strings in Java
+**/
+
+public class mystring{
+    public static void main(String[] args) {
+        
+        int myInt = 7;
+        String text = "Java";
+        String blank = "";
+        String name = "John";
+        String greeting = text + blank + name;
+    }
+}
---
+++ my-project/src/main/java/com/miguelsanchezjr/project/usingvariables.java
@@ -0,0 +1,29 @@
+package com.miguelsanchezjr.project;
+
+
+/* using variables**/
+
+public class usingvariables {
+    public static void main(String[] args){
+        int myNumber = 88;
+        short myShort = 847;
+        long myLong = 9797;
+
+        double myDouble = 7.3542;
+        float myFloat = 324.3f;
+        
+        char myChar = 'y';
+        boolean myBoolean = false;
+
+        byte myByte = 124;
+
+        System.out.print(myNumber);
+        System.out.print(myShort);
+        System.out.print(myLong);
+        System.out.print(myDouble);
+        System.out.print(myFloat);
+        System.out.print(myChar);
+        System.out.print(myBoolean);
+        System.out.print(myByte);
+    }   
+}
